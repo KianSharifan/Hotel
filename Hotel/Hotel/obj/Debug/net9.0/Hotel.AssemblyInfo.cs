32638377ca9ca3cbbf6e2ad2c3b5e844395386e6
Kian <sharifankian@gmail.com>
2026-05-17 02:25:38 +0330

    ERD of project

--- Hotel/Hotel/obj/Debug/net9.0/Hotel.AssemblyInfo.cs
+++ Hotel/Hotel/obj/Debug/net9.0/Hotel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hotel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df64212853764768f094ee669c7b46ca61b7dc60")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a69ef163f2a13a1a846f556eb9f1f3369cd1d622")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hotel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hotel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
