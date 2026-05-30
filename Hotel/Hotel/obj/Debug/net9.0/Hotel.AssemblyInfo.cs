de2da8c478c674c055f4c52d0a2400cd0dc1bb22
Kian <sharifankian@gmail.com>
2026-05-30 19:34:44 +0330

    bug fix

--- Hotel/Hotel/obj/Debug/net9.0/Hotel.AssemblyInfo.cs
+++ Hotel/Hotel/obj/Debug/net9.0/Hotel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hotel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0a08e2b323c5162db96fc364b05f414662f8f19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c77d466eda835e0a809750bdeb2af80e272c5da")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hotel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hotel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
