286fc8c0118b1e3b1abf70db0f17f85d26b47e83
Kian <sharifankian@gmail.com>
2026-05-30 19:44:41 +0330

    Remove generated files from git

--- Hotel/Hotel/obj/Debug/net9.0/Hotel.AssemblyInfo.cs
+++ Hotel/Hotel/obj/Debug/net9.0/Hotel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hotel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de2da8c478c674c055f4c52d0a2400cd0dc1bb22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10f6513659ee5bba1c34a3002c86ddf78e6d811d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hotel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hotel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
