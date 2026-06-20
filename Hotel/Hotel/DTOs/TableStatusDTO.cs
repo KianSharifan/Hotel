f6a62a54172f4ca7cb9ea7ad7732da217a36568c
setayesh <setayesheisazadeh13@gmail.com>
2026-06-20 22:13:06 +0330

    last commit before merge

--- Hotel/Hotel/DTOs/TableStatusDTO.cs
+++ Hotel/Hotel/DTOs/TableStatusDTO.cs
@@ -1,12 +1,4 @@
-// namespace Hotel.DTOs;
-
-// public class TableStatusDTO
-// {
-//     public string? Email;
-//     public string? SpecialReq;
-//     public uint? Capacity;
-//     public DateTime? Time;
-// }
+namespace Hotel.DTOs;
 
 public class TableStatusDTO
 {
