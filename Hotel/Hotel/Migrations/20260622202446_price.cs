bea7071c7f7970e7a2c24e64a4c26f21167e9e4a
Kian <sharifankian@gmail.com>
2026-06-23 00:19:20 +0330

    bug fix

---
+++ Hotel/Hotel/Migrations/20260622202446_price.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hotel.Migrations
+{
+    /// <inheritdoc />
+    public partial class price : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PricePerNight",
+                table: "Rooms");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "PricePerNight",
+                table: "Rooms",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
