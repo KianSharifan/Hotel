cfd4c420303fd315ea2baa625e17b97bf4251cb3
Kian <sharifankian@gmail.com>
2026-07-05 01:09:55 +0330

    bug fix

--- Hotel/Hotel/Controllers/HouseKeepingController.cs
+++ Hotel/Hotel/Controllers/HouseKeepingController.cs
@@ -1,12 +1,37 @@
 using Microsoft.AspNetCore.Mvc;
+using Hotel.Data;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace Hotel.Controllers;
 
+[Route("API/HouseKeeping")]
+[ApiController]
 public class HouseKeepingController : Controller
 {
-    // GET
-    public IActionResult Index()
+    private readonly AppDBContext  _context;
+
+    public HouseKeepingController(AppDBContext context)
     {
-        return View();
+        _context = context;
     }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        try
+        {
+            return Ok(await _context.HouseKeepings.ToListAsync());
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    // [HttpPost]
+    // public async Task<IActionResult> Create()
+    // {
+    //     
+    // }
 }
